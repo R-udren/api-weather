9135912dcf0767620e80ea30342669d0cdae87f4
Robert <54269637+R-udren@users.noreply.github.com>
2023-08-01 15:39:41 +0300

    Update weather.py
    - Added __ifmain__ check
    - Wrapped the code in `weather_report` function
    - Added __docstring__ to main function

--- weather.py
+++ weather.py
@@ -1,12 +1,22 @@
 import requests
 
 
-cities = ['Лондон', 'Аэропорт Шереметьево', 'Череповец']
-payload = {'nmMTq': '',  # тут все нужные параметры отображения и едениц измерений
-           'lang': 'ru'}
-for city in cities:
-    url = f'https://wttr.in/{city}'
-    response = requests.get(url, params=payload)  # params ставит '=' даже когда нет значения, но это ни на что не влияет
-    response.raise_for_status()
+def weather_report():
+    """
+    This function retrieves weather information for each city in the 'cities' list using the wttr.in service,
+    and prints the weather report in the console.
+    """
+    cities = ['Лондон', 'Аэропорт Шереметьево', 'Череповец']
+    payload = {'nmMTq': '',
+               'lang': 'ru'}
+    for city in cities:
+        url = f'https://wttr.in/{city}'
 
-    print(response.text)
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+
+        print(response.text)
+
+
+if __name__ == '__main__':
+    weather_report()
